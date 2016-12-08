5ba34fa342e2746ecf140f2946c53ef52005c729
Joonwoo Park <joonwoop@codeaurora.org>
2016-12-08 13:46:32 -0800

    sched: EAS/WALT: fix deflated energy cost of previous CPU
    WALT subtracts task util from CPU util when a task sleeps.  Therefore
    at the moment of wakeup CPU util doesn't have accounted task util in
    it.  Take such into consideration in order to calculate energy diff
    more preciously.
    
    Change-Id: I9bef40abb551557ebc3382ce5d77c053cfca8c1f
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5052,8 +5052,43 @@ static unsigned long __cpu_norm_util(int cpu, unsigned long capacity, int delta)
 
 static int calc_util_delta(struct energy_env *eenv, int cpu)
 {
+#ifdef CONFIG_SCHED_WALT
+       if (cpu == eenv->src_cpu) {
+               if (!walt_disabled && sysctl_sched_use_walt_task_util &&
+                    eenv->task->state == TASK_WAKING) {
+                       if (eenv->util_delta == 0)
+                               /*
+                                * energy before - calculate energy cost when
+                                * the new task is placed onto src_cpu.  The
+                                * task is not on a runqueue so its util is not
+                                * in the WALT's cr_avg as it's discounted when
+                                * it slept last time.  Hence return task's util
+                                * as delta to calculate energy cost of src_cpu
+                                * as if the new task on it.
+                                */
+                               return task_util(eenv->task);
+                       /*
+                        * energy after - WALT's cr_avg already doesn't have the
+                        * new task's util accounted in.  Thus return 0 delta to
+                        * calculate energy cost of the src_cpu without the
+                        * task's util.
+                        */
+                       return 0;
+               }
+               /*
+                * Task is already on a runqueue for example while load
+                * balancing.  WALT's cpu util already accounted the task's
+                * util.  return 0 delta for energy before so energy calculation
+                * to be done with the task's util accounted, return -task_util
+                * for energy after so the calculation to be doen with
+                * discounted task's util.
+                */
+               return -eenv->util_delta;
+       }
+#else
 	if (cpu == eenv->src_cpu)
 		return -eenv->util_delta;
+#endif
 	if (cpu == eenv->dst_cpu)
 		return eenv->util_delta;
 	return 0;
@@ -5316,6 +5351,7 @@ static inline int __energy_diff(struct energy_env *eenv)
 		.dst_cpu	= eenv->dst_cpu,
 		.nrg		= { 0, 0, 0, 0},
 		.cap		= { 0, 0, 0 },
+		.task		= eenv->task,
 	};
 
 	if (eenv->src_cpu == eenv->dst_cpu)
